78fd8273dda4fbd419dcbf5ae8fbf208dd9a88d3
Levertion <26185209+Levertion@users.noreply.github.com>
2017-11-28 18:58:17 +0000

    Make incorrect literal highlight full string

--- server/parsers/literal.ts
+++ server/parsers/literal.ts
@@ -21,7 +21,7 @@ export const literalArgumentParser: Parser = {
                 }
             }
             // Otherwise, throw an error
-            throw LITERALEXCEPTIONS.IncorrectLiteral.create(begin, reader, properties.key, reader.string.substring(begin, reader.cursor));
+            throw LITERALEXCEPTIONS.IncorrectLiteral.create(begin, reader.cursor + 1, properties.key, reader.string.substring(begin, reader.cursor));
         }
         if (reader.canRead() && reader.peek(1) !== " ") {
             throw LITERALEXCEPTIONS.MissingSpace.create(reader.cursor + 1, reader, reader.string.substring(reader.cursor + 1));
