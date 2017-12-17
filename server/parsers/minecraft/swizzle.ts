ee81db9edc80b7fa969ab2fd159a0df670a53c25
MrYurihi <mryurihi2003@gmail.com>
2017-12-17 08:24:02 -0500

    Added different errors

--- server/parsers/minecraft/swizzle.ts
+++ server/parsers/minecraft/swizzle.ts
@@ -2,7 +2,8 @@ import { StringReader } from "../../string-reader";
 import { CommandSyntaxException, Parser } from "../../types";
 
 const swizzleExceptions = {
-    InvalidSwizzle: new CommandSyntaxException("Invalid swizzle, expected combination of 'x' 'y' and 'z'", "arguments.swizzle.invalid"),
+    InvalidSwizzle: new CommandSyntaxException("Swizzle contains unexpected character %s, expected '%s'", "arguments.swizzle.invalid"),
+    RepeatedChar: new CommandSyntaxException("Swizzle '%s' repeats the character %s", "argument.swizzle.repeat"),
 };
 
 export const swizzleArgumentParser: Parser = {
@@ -13,11 +14,11 @@ export const swizzleArgumentParser: Parser = {
             const c: string = reader.read();
             if (c.match(/[x-z]/)) {
                 if (swizzlearr.includes(c)) {
-                    throw swizzleExceptions.InvalidSwizzle.create(start, reader.cursor);
+                    throw swizzleExceptions.RepeatedChar.create(start, reader.cursor, swizzlearr.join(), c);
                 }
                 swizzlearr.push(c);
             } else {
-                throw swizzleExceptions.InvalidSwizzle.create(start, reader.cursor);
+                throw swizzleExceptions.InvalidSwizzle.create(start, reader.cursor, c, "x, y, or z");
             }
         }
     },
